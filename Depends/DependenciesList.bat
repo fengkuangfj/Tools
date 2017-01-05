955d205339c40bad7e9c26e63773e0044b85f751
fengkuangfj <fly464447966@126.com>
2017-01-05 18:39:28 +0800

    添加脚本

---
+++ Depends/DependenciesList.bat
@@ -0,0 +1,31 @@
+@echo off
+
+echo 请输入需要查询的文件的全路径(支持拖拽) :
+set /p IMAGE_PATH=
+
+set IMAGE_PATH=%IMAGE_PATH:"=%
+
+if exist "%VS90COMNTOOLS%vsvars32.bat" (
+call "%VS90COMNTOOLS%vsvars32.bat"
+) else (
+if exist "%VS100COMNTOOLS%vsvars32.bat" (
+call "%VS100COMNTOOLS%vsvars32.bat"
+) else (
+if exist "%VS110COMNTOOLS%vsvars32.bat" (
+call "%VS110COMNTOOLS%vsvars32.bat"
+) else (
+if exist "%VS120COMNTOOLS%vsvars32.bat" (
+call "%VS120COMNTOOLS%vsvars32.bat"
+) else (
+if exist "%VS140COMNTOOLS%vsvars32.bat" (
+call "%VS140COMNTOOLS%vsvars32.bat"
+)
+)
+)
+)
+)
+
+del /f /q /a "DependenciesList.log"
+
+rem dumpbin /dependents "%IMAGE_PATH%" >"DependenciesList.log"
+dumpbin /imports "%IMAGE_PATH%" >"DependenciesList.log"
